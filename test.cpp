85cd563e84eabe643883124865640cd473e77d8d
softscrape <softscrape@gmail.com>
2018-08-17 11:13:35 +0300

    Added new API entries

--- test.cpp
+++ test.cpp
@@ -25,20 +25,44 @@ int main(int argc, char *argv[])
     cout << "Private key "    << private_key  << endl;
     cout << "Public key "     << public_key   << endl;
     cout << "Wallet address " << mh_address   << endl;
-    
+
     vector<unsigned char> signature;
 
     string text = "Hello #MetaHash!";
     cout << "Signing text \"" << text << "\"" << endl;
     CRYPTO_sign_text(signature, private_key, text);
-    
+
     cout << "Verifying signature" << endl;
     if(CRYPTO_check_sign_text(signature, public_key, text))
       cout << "Signing test pass" << endl;
     else
       cout << "Signing test fail" << endl;
-  } catch(exception &e) {
 
+    cout << "----------------------------" << endl;
+
+    cout << "Generate public from private" << endl;
+    string public_key_new;
+    CRYPTO_generate_public(private_key, public_key_new);
+    if(public_key == public_key_new)
+      cout << "Public key match" << endl;
+    else
+      cout << "Public key does not match" << endl;
+
+    cout << "Generate address from public" << endl;
+    string mh_address_new;
+    CRYPTO_generate_address(public_key_new, mh_address_new);
+    if(mh_address == mh_address_new)
+      cout << "Address match" << endl;
+    else
+      cout << "Address does not match" << endl;
+
+    cout << "Verifying address" << endl;
+    if(CRYPTO_check_address(mh_address_new))
+      cout << "Address good" << endl;
+    else
+      cout << "Address bad" << endl;
+
+  } catch(exception &e) {
     cerr << "Caught exception: " << e.what() << endl;
     return -1;
   }
